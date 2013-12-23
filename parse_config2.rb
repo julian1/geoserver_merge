bfd0b76565a9494bd21ebc594032a830673da415
julian1 <julian.atkinson71@gmail.com>
2013-12-23 16:08:02 +1100

    update

--- parse_config2.rb
+++ parse_config2.rb
@@ -90,7 +90,6 @@ def trace_oid( oids, oid, depth, &block )
     node = object[:doc]
     path = object[:path]
 
-
     if REXML::XPath.first( node, "/layer" )
       puts "#{get_pad(depth)} *layer #{path}" 
       puts "#{get_pad(depth+1)} +name->#{REXML::XPath.first( node, '/layer/name').text}"
@@ -104,6 +103,61 @@ def trace_oid( oids, oid, depth, &block )
       puts "#{get_pad(depth+1)} +enabled->#{REXML::XPath.first( node, '/featureType/enabled').text}"
     end
 
+    if REXML::XPath.first( node, "/namespace" )
+      puts "#{get_pad(depth)} *namespace#{path}" 
+      puts "#{get_pad(depth+1)} +prefix->#{REXML::XPath.first( node, '/namespace/prefix').text}"
+    end
+
+    if REXML::XPath.first( node, "/dataStore" )
+      puts "#{get_pad(depth)} *dataStore#{path}" 
+      puts "#{get_pad(depth+1)} +name->#{REXML::XPath.first( node, '/dataStore/name').text}"
+
+      type = REXML::XPath.first( node, '/dataStore/type') 
+      if type
+        puts "#{get_pad(depth+1)} +type->#{type.text}"
+      end
+
+      # a dataStore with a reference to a shapefile 
+      url = REXML::XPath.first( node, "/dataStore/connectionParameters/entry[@key='url']" )
+      if url
+        puts "#{get_pad(depth+1)} +url #{url.text} "
+      end
+
+      jndi = REXML::XPath.first( node, "/dataStore/connectionParameters/entry[@key='jndiReferenceName']") 
+      if jndi
+        puts "#{get_pad(depth+1)} +jndi #{jndi.text} "
+      end
+
+      schema = REXML::XPath.first( node, "/dataStore/connectionParameters/entry[@key='schema']") 
+      if schema
+        puts "#{get_pad(depth+1)} +schema #{schema.text} "
+      end
+
+
+
+
+    end
+
+
+    if REXML::XPath.first( node, "/style" )
+      puts "#{get_pad(depth)} *style#{path}" 
+      puts "#{get_pad(depth+1)} +name->#{REXML::XPath.first( node, '/style/name').text}"
+
+      # if it's a style with a ref to a stylefile 
+      style_file = REXML::XPath.first( node, "/style/filename" )
+      if style_file
+        fullpath = "#{File.dirname( object[:path] )}/#{style_file.text}"
+        if File.exists?( fullpath)
+            puts "#{get_pad(depth + 1)} STYLEFILE #{fullpath}" 
+        else
+            abort( "missing style file")
+        end
+      end
+
+
+    end
+
+
 
 
     # call our block to perform the processing
@@ -114,23 +168,6 @@ def trace_oid( oids, oid, depth, &block )
     # and process them
     REXML::XPath.each( object[:doc], "/*/*/id" ) do |e|
       trace_oid( oids, e.text , depth + 1, &block )
-    end
-
-    # if it's a style with a ref to a stylefile 
-    style_file = REXML::XPath.first( object[:doc], "/style/filename" )
-    if style_file
-      fullpath = "#{File.dirname( object[:path] )}/#{style_file.text}"
-      if File.exists?( fullpath)
-          puts "#{get_pad(depth + 1)} STYLEFILE #{fullpath}" 
-      else
-          puts "**MISSING**"
-      end
-    end
-
-    # a dataStore with a reference to a shapefile 
-    url = REXML::XPath.first( object[:doc], "/dataStore/connectionParameters/entry[@key='url']" )
-    if url
-      puts "**** WHOOT a datastore with a url #{url.text} "
     end
 
 
