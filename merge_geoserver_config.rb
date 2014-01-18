0db0603c13c2fb703f32791006d7edfb8a9ef491
julian1 <julian.atkinson71@gmail.com>
2014-01-18 14:11:32 +1100

    update

--- merge_geoserver_config.rb
+++ merge_geoserver_config.rb
@@ -228,27 +228,53 @@ def begin_trace_oids( oids, options )
   # we want to refactor this to avoid using the loop...
 
 
-#  layers = 
+  puts "******here1"
 
-  # start tracing from the layer root keys
-  oids.keys.each() do |oid|
+  layer_keys = oids.keys.select() { |oid|
 
-    # keep going unless this object is a layer file? 
+#    |layer| layer[:name] == options[:remove] 
+
     layer_name = nil
     oids[ oid].each() do |object|
       layer_name = REXML::XPath.first( object[:xml], "/layer/name" )
+      puts "#{layer_name}"
     end
-    next unless layer_name
+    layer_name
+  } 
 
+  puts "******here2"
+
+  layer_keys.each() do | oid |
     # do the scan
     files = {} 
     other_files = []
     trace_oid( oids, oid, 0, options, files, other_files )
 
-
     yield files, other_files
 
   end
+
+  puts "******here3"
+
+#   # start tracing from the layer root keys
+#   oids.keys.each() do |oid|
+# 
+#     # keep going unless this object is a layer file? 
+#     layer_name = nil
+#     oids[ oid].each() do |object|
+#       layer_name = REXML::XPath.first( object[:xml], "/layer/name" )
+#     end
+#     next unless layer_name
+# 
+#     # do the scan
+#     files = {} 
+#     other_files = []
+#     trace_oid( oids, oid, 0, options, files, other_files )
+# 
+# 
+#     yield files, other_files
+# 
+#   end
 end
 
 
