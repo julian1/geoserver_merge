5a652cdcad3416dbb6475946bc293ab5e93b203a
julian1 <julian.atkinson71@gmail.com>
2014-05-07 11:36:09 +1000

    update

--- cross_reference.rb
+++ cross_reference.rb
@@ -15,44 +15,35 @@ Find.find(tmp_dir) do |path|
 	# skip any path that doesn't have a .xml extension
 	next unless File.extname(path) == '.xml'
 
-	# decode to xml once
-	@xml = Nokogiri::XML(File.open(path))
+	# decode to xml
+	xml = Nokogiri::XML(File.open(path))
 
 
 	# layer objects
-	layer_id = @xml.at_xpath("/layer/id")
+	layer_id = xml.at_xpath("/layer/id")
 	if layer_id
-		name = @xml.at_xpath("/layer/name")
+		name = xml.at_xpath("/layer/name")
 		raise "layer missing id" unless name
-		feature_id = @xml.at_xpath("/layer/resource/id")
+		feature_id = xml.at_xpath("/layer/resource/id")
 		raise "layer missing feature id" unless feature_id
 
 		puts "layer -> #{name.inner_html}, id #{layer_id.inner_html}, feature_id #{feature_id.inner_html}"
 	end
 
-#	<featureType>
-#  <id>FeatureTypeInfoImpl-2d23ec69:126d477e611:-7e38</id>
-
-	feature_id = @xml.at_xpath("/featureType/id")
+	feature_id = xml.at_xpath("/featureType/id")
 	if feature_id 
-		# we don't even need the name
-
- 		name = @xml.at_xpath("/featureType/name")
+ 		name = xml.at_xpath("/featureType/name")
  		raise "feature missing name" unless name
- 	
 
 		puts "feature #{feature_id.inner_html}, name #{name.inner_html}"
 	end
 
 
 #	//xpath_object = load_xml_file(path) #xml_file).at_xpath(path)
-
 # 	layer_name = Chef::Recipe::XMLHelper::get_xml_value(file, "layer/name")
 # 	if( layer_name)
 #         #next unless layer_name
-# 
 # 	end
-
 #	puts path 
 
 
