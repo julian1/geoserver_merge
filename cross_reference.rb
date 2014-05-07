214e2abd5f4b013868c48bffcd6ec203779e3fd5
julian1 <julian.atkinson71@gmail.com>
2014-05-07 11:28:14 +1000

    update

--- cross_reference.rb
+++ cross_reference.rb
@@ -13,18 +13,28 @@ Find.find(tmp_dir) do |path|
 	# skip directories
 	next unless FileTest.file?(path)
 	# skip any path that doesn't have a .xml extension
-
 	next unless File.extname(path) == '.xml'
 
-
+	# decode to xml once
 	@xml = Nokogiri::XML(File.open(path))
 
-#	 xml_node = @xml.at_xpath("#{xpath}/#{name}")
 
-	layer_node = @xml.at_xpath("/layer/name")
+	# layer objects
+	layer = @xml.at_xpath("/layer/name")
+	if layer
+		id = @xml.at_xpath("/layer/id")
+		raise "missing id" unless id
+		# assert have id
+		puts "layer -> #{layer.inner_html}, id #{id.inner_html}"
+		# need to get the id...
+		
+	end
 
-	if layer_node 
-		puts layer_node.inner_html
+#	<featureType>
+#  <id>FeatureTypeInfoImpl-2d23ec69:126d477e611:-7e38</id>
+	feature_type = @xml.at_xpath("/featureType/name")
+	if feature_type
+#		puts feature_type.inner_html
 	end
 
 
